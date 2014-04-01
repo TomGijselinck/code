8f3475fe9db425be1817a9738bfc4ee169a3012a
tomgijselinck <tomgijselinck@30dcf944-c4f8-45d0-ad98-8bdb4daa881f>
2014-04-01 17:19:28 +0000

    Added JUnit test case for position class.

---
+++ branches/release-1.1/tests/worms/model/PositionTest.java
@@ -0,0 +1,84 @@
+package worms.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import worms.exceptions.IllegalCoordinateException;
+import static worms.util.Util.*;
+
+/**
+ * A class collecting tests for the class of positions.
+ * 
+ * @version 1.0
+ * @author Tom Gijselinck
+ * 
+ */
+
+public class PositionTest {
+
+	/**
+	 * Variable referencing a position with X coordinate 1 and Y coordinate 2.
+	 */
+	private static Position position1x1y;
+	
+	/**
+	 * Set up an immutable test fixture.
+	 * 
+	 * @post	The variable position1x1y references a new position with X
+	 * 			coordinate 1 and Y coordinate 2.
+	 */
+	@BeforeClass
+	public static void setUpImmutableFixture() {
+		position1x1y = new Position(1, 1);
+	}
+	
+	@Test
+	public void constructor_LegalCase() {
+		Position position = new Position(2, 2);
+		assertTrue(fuzzyEquals(2, position.getX()));
+		assertTrue(fuzzyEquals(2, position.getY()));
+	}
+	
+	@Test (expected = IllegalCoordinateException.class)
+	public void constructor_IllegalXCoordinate() {
+		new Position(Double.NaN, 1);
+	}
+	
+	@Test (expected = IllegalCoordinateException.class)
+	public void constructor_IllegalYCoordinate() {
+		new Position(1, Double.NaN);
+	}
+	
+	@Test
+	public void translate_SingleCase() {
+		Position translatedPosition = position1x1y.translate(1, 2);
+		assertTrue(fuzzyEquals(2, translatedPosition.getX()));
+		assertTrue(fuzzyEquals(3, translatedPosition.getY()));
+	}
+	
+	@Test
+	public void equals_TrueCase() {
+		Position otherPosition = new Position(1, 1);
+		assertTrue(position1x1y.equals(otherPosition));
+	}
+	
+	@Test
+	public void equals_DifferentClass() {
+		double otherObject = 5;
+		assertFalse(position1x1y.equals(otherObject));
+	}
+
+	@Test
+	public void equals_DifferentXCoordinate() {
+		Position otherPosition = new Position(5, 1);
+		assertFalse(position1x1y.equals(otherPosition));
+	}
+	
+	@Test
+	public void equals_DifferentYCoordinate() {
+		Position otherPosition = new Position(1, 5);
+		assertFalse(position1x1y.equals(otherPosition));
+	}
+
+}

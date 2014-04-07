449d4e7c429ad048585ceecd4b2f0be44c6a4528
tomgijselinck <tomgijselinck@30dcf944-c4f8-45d0-ad98-8bdb4daa881f>
2014-04-07 09:29:19 +0000

    Added specifications to class 'World'. (not finished!)

--- trunk/src/worms/model/World.java
+++ trunk/src/worms/model/World.java
@@ -1,7 +1,15 @@
 package worms.model;
 
+import be.kuleuven.cs.som.annotate.*;
 
 /**
+ * ...
+ * 
+ * @invar	...
+ * 		  |	isValidWidth(getWidth())
+ * @invar	...
+ * 		  |	isValidHeight(getHeight())
+ * 	 
  * @author 	Tom Gijselinck
  * @version	1.0
  *
@@ -9,20 +17,142 @@ package worms.model;
 public class World {
 	
 	//CONSTRUCTORS
+	
+	/**
+	 * ...
+	 * 
+	 * @param 	width
+	 * 			...
+	 * @param 	height
+	 * 			...
+	 * @param 	passableMap
+	 * 			...
+	 * @post	...
+	 * 		  |	new.getWidth() == width
+	 * @post	...
+	 * 		  |	new.getHeight() == height
+	 * @post	...
+	 * 		  |	new.getPassableMap() == passableMap
+	 * @throws	IllegalArgumentException
+	 * 		  	...
+	 * 		  |	! isValidWidth(width)
+	 * @throws	IllegalArgumentException
+	 * 		  	...
+	 * 		  |	! isValidHeight(height)
+	 */
 	public World(double width, double height, boolean[][] passableMap) {}
 	
-	public double getWidth() { return 0;}
-	private void setWidth() {}
 	
+	
+	
+	/**
+	 * Return the width of this world.
+	 */
+	@Basic
+	public double getWidth() { return 0;}
+	
+	/**
+	 * Checks whether the given width is a valid width for any world.
+	 * 
+	 * @return	...
+	 * 		  |	result ==
+	 * 		  |		width <= World.getWidthUpperBound()
+	 */
+	public boolean isValidWidth() { return true;}
+	
+	/**
+	 * Set the width of this world to the given width.
+	 * 
+	 * @param	width
+	 * 			...
+	 * @throws	IllegalArgumentException
+	 * 			...
+	 * 		  |	! isValidWidth(width)
+	 */
+	private void setWidth(double width) {}
+	
+	
+	
+	
+	/**
+	 * Return the width upper bound for any world.
+	 */
+	@Basic
 	public static double getWidthUpperBound() { return 0;}
 	
+	
+	
+	
+	/**
+	 * Return the height of this worm.
+	 */
+	@Basic
 	public double getHeight() { return 0;}
+	
+	/**
+	 * Checks whether the given height is a valid height for any world.
+	 * 
+	 * @return	...
+	 * 		  |	result ==
+	 * 		  |		height <= World.getHeightUpperBound()
+	 */
+	public boolean isValidHeight() { return true;}
+	
+	/**
+	 * Set the height of this world to the given height.
+	 * 
+	 * @param	height
+	 * 			...
+	 * @throws	IllegalArgumentException
+	 * 			...
+	 * 		  |	! isValidHeight(height)
+	 */
 	private void setHeight() {}
 	
+	
+	
+	
+	/**
+	 * Return the height upper bound for any world.
+	 */
+	@Basic
 	public static double getheighthUpperBound() { return 0;}
 	
-	public boolean isPassableTerrain() { return true;}
-	public boolean isAdjacentToPassableTerrain() { return true;}
+	
+	
+	
+	/**
+	 * Return the passable map of this world.
+	 */
+	private boolean[][] getPassableMap() { return new boolean[1][1];}
+	
+	
+	
+	
+	/**
+	 * Checks whether the given location is passable.
+	 * 
+	 * @return	...
+	 * 		  |	let
+	 * 		  |		passableMap = getPassableMap()
+	 * 		  |		&& x = location.getX()
+	 * 		  |		&& y = location.getY()
+	 * 		  |	in
+	 * 		  |		passableMap[x][y] == true
+	 */
+	public boolean isPassableTerrain(Position location) { return true;}
+	
+	/**
+	 * Checks whether the given location is passable and adjacent to an
+	 * impassable location.
+	 * 
+	 * @return	...
+	 * 		  |	let 
+	 */
+	public boolean isAdjacent() { return true;}
+	
+	
+	
 	
 	public void startGame() {}
 	

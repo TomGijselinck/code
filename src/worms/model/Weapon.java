aab7f2902302e99531a255e05f2d889bb632a14a
tomgijselinck <tomgijselinck@30dcf944-c4f8-45d0-ad98-8bdb4daa881f>
2014-04-09 17:32:29 +0000

    Changed 'Weapon' class to an enumeration. Added specification and implementation for 'Weapon' class.

--- src/worms/model/Weapon.java
+++ src/worms/model/Weapon.java
@@ -1,24 +1,66 @@
 package worms.model;
 
+import be.kuleuven.cs.som.annotate.Basic;
+import be.kuleuven.cs.som.annotate.Immutable;
+import be.kuleuven.cs.som.annotate.Value;
+
 
 /**
- * ...
+ * An enumeration introducing different weapons.
+ * 	In its current form, the class only supports the Rifle and the Bazooka.
  * 	 
  * @author 	Tom Gijselinck
  * @version	1.0
  *
  */
-public class Weapon {
+@Value
+enum Weapon {
 	
-	public Weapon(int ActionPointCost) {}
+	RIFLE("Rifle", 10), BAZOOKA("Bazooka", 50);
 	
-	//DESTRUCTOR
-	public void terminate() {}
-	public boolean isTerminated() { return isTerminated;}
-	private boolean isTerminated;
+	/**
+	 * Initialize this weapon with given name and action point cos.
+	 * 
+	 * @param 	name
+	 * 			...
+	 * @param 	ActionPointCost
+	 * 			...
+	 * @post	...
+	 * 		  |	new.getName() == name
+	 * @post	...
+	 * 		  |	new.getActionPointCost() == actionPointCost
+	 */
+	private Weapon(String name, int ActionPointCost) {
+		this.name = name;
+		this.actionPointCost = ActionPointCost;
+	}
 	
-	public String getName() { return "null";}
+	/**
+	 * ...
+	 */
+	@Basic @Immutable
+	public String getName() {
+		return name;
+	}
 	
-	public int getActionPointsCost() { return 0;}
+	/**
+	 * ...
+	 */
+	private final String name;
+	
+	
+	
+	
+	/**
+	 * ...
+	 */
+	public int getActionPointsCost() {
+		return actionPointCost;
+	}
+	
+	/**
+	 * ...
+	 */
+	private int actionPointCost;
 
 }

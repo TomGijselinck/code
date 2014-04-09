7b33c9441c6b9abccbbcb9b79083f779db87ec99
tomgijselinck <tomgijselinck@30dcf944-c4f8-45d0-ad98-8bdb4daa881f>
2014-04-09 09:41:05 +0000

    Added 'Projectile' class and 'Weapon' class. Included necessary methods for restricted bidirectional association between 'World' and 'Projectile'. Added specification (incomplete).

---
+++ src/worms/model/Weapon.java
@@ -0,0 +1,19 @@
+package worms.model;
+
+
+/**
+ * ...
+ * 	 
+ * @author 	Tom Gijselinck
+ * @version	1.0
+ *
+ */
+public class Weapon {
+	
+	public Weapon(int ActionPointCost) {}
+	
+	public String getName() { return "null";}
+	
+	public int getActionPointsCost() { return 0;}
+
+}

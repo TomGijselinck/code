f22bd117971bb8bd456a0e1236c192e1d78e9caf
tomgijselinck <tomgijselinck@30dcf944-c4f8-45d0-ad98-8bdb4daa881f>
2014-05-13 16:02:08 +0000

    Added files part 3.

--- tests-provided/worms/model/PartialFacadeTest.java
+++ tests-provided/worms/model/PartialFacadeTest.java
@@ -1,14 +1,18 @@
 package worms.model;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Random;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import worms.model.Facade;
-import worms.model.Worm;
+import worms.gui.game.IActionHandler;
+import worms.model.programs.ParseOutcome;
+import worms.model.programs.ParseOutcome.Success;
 import worms.util.Util;
 
 public class PartialFacadeTest {
@@ -20,8 +24,6 @@ public class PartialFacadeTest {
 	private Random random;
 
 	private World world;
-	
-	private World world1;
 
 	// X X X X
 	// . . . .
@@ -30,50 +32,23 @@ public class PartialFacadeTest {
 	private boolean[][] passableMap = new boolean[][] {
 			{ false, false, false, false }, { true, true, true, true },
 			{ true, true, true, true }, { false, false, false, false } };
-	
-	// 10x10 pixels
-			//     0 1 2 3 4 5 6 7 8 9
-			//    --------------------
-			//  1| . . . . . . . . . .
-			//  2| . . X . . . . . . .
-			//  3| X X X . . . . X . .
-			//  4| . X X X X X X X . .
-			//  5| . . . . . . . . . .
-			//  6| . . . . . . . . . .
-			//  7| . . . . . . O . . .
-			//  8| . . . . . . . . . .
-			//  9| . . . . . . . . . .
-			// 10| X X X X X X X X X X
-			private boolean[][] passableMap1 = new boolean[][] {
-					{true, 	true, 	true, 	true,	true,	true,	true,	true,	true,	true},
-					{true, 	true, 	false, 	true,	true,	true,	true,	true,	true,	true},
-					{false, false, 	false, 	true,	true,	true,	true,	false,	true,	true},
-					{true, false, 	false, 	false,	false,	false,	false,	false,	true,	true},
-					{true, 	true, 	true, 	true,	true,	true,	true,	true,	true,	true},
-					{true, 	true, 	true, 	true,	true,	true,	true,	true,	true,	true},
-					{true, 	true, 	true, 	true,	true,	true,	true,	true,	true,	true},
-					{true, 	true, 	true, 	true,	true,	true,	true,	true,	true,	true},
-					{true, 	true, 	true, 	true,	true,	true,	true,	true,	true,	true},
-					{false, false, 	false, 	false,	false,	false,	false,	false,	false,	false}
-			};
 
 	@Before
 	public void setup() {
 		facade = new Facade();
 		random = new Random(7357);
-		world = new World(4.0, 4.0, passableMap, random);
-		world1 = new World(5, 5, passableMap1, new Random());
+		world = facade.createWorld(4.0, 4.0, passableMap, random);
 	}
 
 	@Test
 	public void testMaximumActionPoints() {
-		Worm worm = facade.createWorm(world, 1, 2, 0, 1, "Test");
+		Worm worm = facade.createWorm(world, 1, 2, 0, 1, "Test", null);
 		assertEquals(4448, facade.getMaxActionPoints(worm));
 	}
 
 	@Test
 	public void testMoveHorizontal() {
-		Worm worm = facade.createWorm(world, 1, 2, 0, 1, "Test");
+		Worm worm = facade.createWorm(world, 1, 2, 0, 1, "Test", null);
 		facade.move(worm);
 		assertEquals(2, facade.getX(worm), EPS);
 		assertEquals(2, facade.getY(worm), EPS);
@@ -81,7 +56,8 @@ public class PartialFacadeTest {
 
 	@Test
 	public void testMoveVertical() {
-		Worm worm = facade.createWorm(world, 1, 1.5, Math.PI / 2, 0.5, "Test");
+		Worm worm = facade.createWorm(world, 1, 1.5, Math.PI / 2, 0.5, "Test",
+				null);
 		facade.move(worm);
 		assertEquals(1, facade.getX(worm), EPS);
 		assertEquals(2.0, facade.getY(worm), EPS);
@@ -94,7 +70,7 @@ public class PartialFacadeTest {
 		World world = facade.createWorld(3.0, 2.0, new boolean[][] {
 				{ true, true, false }, { true, true, false } }, random);
 		Worm worm = facade.createWorm(world, 1.5, 0.5,
-				Math.PI / 2 - 10 * 0.0175, 0.5, "Test");
+				Math.PI / 2 - 10 * 0.0175, 0.5, "Test", null);
 		facade.move(worm);
 		assertEquals(1.5, facade.getX(worm), EPS);
 		assertEquals(1.0, facade.getY(worm), EPS);
@@ -110,17 +86,30 @@ public class PartialFacadeTest {
 				{ true, false, true }, { true, true, true },
 				{ true, true, true }, { false, false, false } }, random);
 		Worm worm = facade.createWorm(world, 1.5, 2.5, -Math.PI / 2, 0.5,
-				"Test");
+				"Test", null);
 		assertFalse(facade.canFall(worm));
 		facade.move(worm);
 		assertTrue(facade.canFall(worm));
 		facade.fall(worm);
 		assertEquals(1.5, facade.getX(worm), EPS);
-		assertEquals(1.5, facade.getY(worm), EPS);
-	}
+		assertTrue("Worm must land at adjacent location",
+				Util.fuzzyBetween(1.5, 1.55, facade.getY(worm), EPS));
+	};
 	
 	@Test
-	public void isAdjacent_TrueCase() {
-		assertFalse(facade.isAdjacent(world1, 0.5, 1.5, 0.5));
+	public void testProgram() {
+		IActionHandler handler = new SimpleActionHandler(facade);
+		World world = facade.createWorld(100.0, 100.0, new boolean[][] { {true}, {false} }, random);
+		ParseOutcome<?> outcome = facade.parseProgram("double x; while (x < 1.5) {\nx := x + 0.1;\n}\n turn x;", handler);
+		assertTrue(outcome.isSuccess());
+		Program program = ((Success)outcome).getResult();
+		Worm worm = facade.createWorm(world, 50.0, 50.51, 0, 0.5, "Test", program);
+		facade.addNewWorm(world, null); // add another worm
+		double oldOrientation = facade.getOrientation(worm);
+		facade.startGame(world); // this will run the program
+		double newOrientation = facade.getOrientation(worm);
+		assertEquals(oldOrientation + 1.5, newOrientation, EPS);
+		assertNotEquals(worm, facade.getCurrentWorm(world)); // turn must end after executing program
 	}
+
 }
